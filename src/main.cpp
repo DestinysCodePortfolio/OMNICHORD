49d03c1efa894df3ab5ecc663c3edd45976fef2f
DestinysCodePortfolio <Destinygonzalez2904@gmail.com>
2025-12-09 20:07:14 -0800

    -a

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,13 @@ int j;
 int flag = 0;
 
 chord currChord = quiet;
-int sample = 0;
+unsigned long sample = 0;
 
-//Task struct for concurrent synchSMs implmentations
 typedef struct _task{
-	signed 	 char state; 		//Task's current state
-	unsigned long period; 		//Task period
-	unsigned long elapsedTime; 	//Time elapsed since last task tick
-	int (*TickFct)(int); 		//Task tick function
+	signed 	 char state;
+	unsigned long period;
+	unsigned long elapsedTime;
+	int (*TickFct)(int);
 } task;
 
 
@@ -38,7 +37,6 @@ enum chord_states { chords_init, silence, play_chord };
 int TickFct_chords(int state) {
     switch(state) {
         case chords_init:
-
             state = silence;
             break;
 
@@ -47,17 +45,18 @@ int TickFct_chords(int state) {
            if (matrixPressed) {
             state = play_chord;
            }
-          
             break;
 
         case play_chord:
            matrix_read();
-         state = play_chord;
-        
+           if (!matrixPressed) {
+               state = silence;
+               currChord = quiet;
+           } else {
+               state = play_chord;
+           }
         break;
 
-
-        
         default:
             break;
     }
@@ -71,62 +70,62 @@ int TickFct_chords(int state) {
 
         case play_chord:
     
-        if (currentKey == Eb  )  {
+        if (currentKey == Eb)  {
           currChord = Ebmaj;
 
-        } else if (currentKey == Bb ) {
+        } else if (currentKey == Bb) {
           currChord = Bbmaj;
 
-        } else if (currentKey == F ) {
+        } else if (currentKey == F) {
           currChord = Fmaj;
 
-        } else if (currentKey == C ) {
+        } else if (currentKey == C) {
           currChord = Cmaj;
 
-        } else if (currentKey == G ) {
+        } else if (currentKey == G) {
           currChord = Gmaj;
 
-        } else if (currentKey == D ) {
+        } else if (currentKey == D) {
           currChord = Dmaj;
 
-        } else if (currentKey == A ) {
+        } else if (currentKey == A) {
           currChord = Amaj;
 
-        } else if (currentKey == E ) {
+        } else if (currentKey == E) {
           currChord = Emaj;
 
-        } else if (currentKey == B ) {
+        } else if (currentKey == B) {
           currChord = Bmaj;
 
         } else if (currentKey == Ebm) {
           currChord = Ebmin;
 
-        } else if (currentKey == Bbm ) {
+        } else if (currentKey == Bbm) {
           currChord = Bbmin;
 
-        } else if (currentKey == Fm ) {
+        } else if (currentKey == Fm) {
           currChord = Fmin;
 
-        } else if (currentKey == Cm ) {
+        } else if (currentKey == Cm) {
           currChord = Cmin;
 
         } else if (currentKey == Gm) {
           currChord = Gmin;
 
-        } else if (currentKey == Dm ) {
+        } else if (currentKey == Dm) {
           currChord = Dmin;
 
-        } else if (currentKey == Em ) {
+        } else if (currentKey == Em) {
           currChord = Emin;
 
-        } else if (currentKey == Bm ) {
+        } else if (currentKey == Bm) {
           currChord = Bmin;
 
         } else if (currentKey == Am) {
           currChord = Amin;
 
-        } else if (currentKey == drums ) {
-          drumsOn = true;
+        } else if (currentKey == drums) {
+          drumsOn = !drumsOn;
         }
 
         break;
@@ -153,8 +152,6 @@ int TickFct_strumplate(int state) {
           state = strum;
           break;
 
-
-        
         default:
           break;
     }
@@ -188,7 +185,6 @@ int TickFct_drums(int state) {
         } else {
           state = idle_drums;
         }
-
             break;
 
         case playDrums:
@@ -197,11 +193,8 @@ int TickFct_drums(int state) {
           } else {
             state = playDrums;
           }
-        
         break;
 
-
-        
         default:
             break;
     }
@@ -218,16 +211,13 @@ int TickFct_drums(int state) {
         
         if (step == 4 || step == 12) {
           snareHit = 1.0;
-    
         }
         
-        step = (step + 1 ) % 16 ;
+        step = (step + 1) % 16;
         break;
 
         default:
         break;
-
-
     }
     return state;
 }
@@ -252,8 +242,6 @@ int TickFct_button(int state) {
           state = reset_release;
           break;
 
-
-        
         default:
           break;
     }
@@ -278,100 +266,70 @@ int TickFct_button(int state) {
 }
 
 void TimerISR() {
-	for ( unsigned int i = 0; i < NUM_TASKS; i++ ) {                   // Iterate through each task in the task array
-		if ( tasks[i].elapsedTime == tasks[i].period ) {           // Check if the task is ready to tick
-			tasks[i].state = tasks[i].TickFct(tasks[i].state); // Tick and set the next state for this task
-			tasks[i].elapsedTime = 0;                          // Reset the elapsed time for the next tick
+	for (unsigned int i = 0; i < NUM_TASKS; i++) {
+		if (tasks[i].elapsedTime == tasks[i].period) {
+			tasks[i].state = tasks[i].TickFct(tasks[i].state);
+			tasks[i].elapsedTime = 0;
 		}
-		tasks[i].elapsedTime += GCD_PERIOD;                        // Increment the elapsed time by GCD_PERIOD
+		tasks[i].elapsedTime += GCD_PERIOD;
 	}
 }
 
-ISR(TIMER1_COMPA_vect) {
-
-    double s = mixing(currChord, sample++);   
-
+ISR(TIMER0_COMPA_vect) {
+    double s = mixing(currChord, sample++);
+    
     double v = (s + 1.0) * (ICR1 / 2.0);
-    if (v < 0.0) {
-      v = 0.0;
-    } 
-    if (v > ICR1) {
-       v = ICR1;
-    }
-
-    OCR1A = v;
+    
+    if (v < 0.0) v = 0.0;
+    if (v > ICR1) v = ICR1;
+    
+    OCR1A = (uint16_t)v;
 }
 
 
-
-
-
 int main(void) {
-    //TODO: initialize all your inputs and ouputs
-  DDRB  =  0x00;
-  PORTB =  0xFF;
+    DDRC  = 0xFF;
+    PORTC = 0x00;
 
-  DDRC  =  0xFF;
-  PORTC =  0x00;
-
-  DDRD  =  0x00;
-  PORTD =  0xFF;
-    ADC_init();   // initializes ADC
-    matrix_init(); // initalizes matrix
-  
-
-
-   
-
-    TCCR1A |= (1 << WGM11) | (1 << COM1A1); //COM1A1 sets it to channel A
-    TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11); //CS11 sets the prescaler to be 8
-    //WGM11, WGM12, WGM13 set timer to fast pwm mode
-
-    ICR1 = 90; 
-    OCR1A = 45;                // 50%
-    TIMSK1 |= (1 << OCIE1A);   // enable audio interrupt
-
-
-
-
-    //TODO: Initialize tasks here
-    // e.g. 
-    // tasks[0].period = ;
-    // tasks[0].state = ;
-    // tasks[0].elapsedTime = ;
-    // tasks[0].TickFct = ;
-
-  tasks[0].period = TASK1_PERIOD;
-  tasks[0].state = chords_init;
-  tasks[0].elapsedTime = TASK1_PERIOD;
-  tasks[0].TickFct = &TickFct_chords;
-
-  
-  tasks[1].period = TASK2_PERIOD;
-  tasks[1].state = strum_init;
-  tasks[1].elapsedTime = TASK2_PERIOD;
-  tasks[1].TickFct = &TickFct_strumplate;
-
+    ADC_init();
+    matrix_init();
     
-  tasks[2].period = TASK3_PERIOD;
-  tasks[2].state = drum_init;
-  tasks[2].elapsedTime = TASK3_PERIOD;
-  tasks[2].TickFct = &TickFct_drums;
+    DDRB |= (1 << PB1);
+    
+    TCCR1A |= (1 << WGM11) | (1 << COM1A1);
+    TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11);
+    
+    ICR1 = 200;
+    OCR1A = 100;
+    
+    TCCR0A = (1 << WGM01);
+    TCCR0B = (1 << CS01);
+    OCR0A = 99;
+    TIMSK0 = (1 << OCIE0A);
 
-      
-  tasks[3].period = TASK4_PERIOD;
-  tasks[3].state = button_init;
-  tasks[3].elapsedTime = TASK4_PERIOD;
-  tasks[3].TickFct = &TickFct_button;
+    tasks[0].period = TASK1_PERIOD;
+    tasks[0].state = chords_init;
+    tasks[0].elapsedTime = TASK1_PERIOD;
+    tasks[0].TickFct = &TickFct_chords;
 
-  
+    tasks[1].period = TASK2_PERIOD;
+    tasks[1].state = strum_init;
+    tasks[1].elapsedTime = TASK2_PERIOD;
+    tasks[1].TickFct = &TickFct_strumplate;
 
+    tasks[2].period = TASK3_PERIOD;
+    tasks[2].state = drum_init;
+    tasks[2].elapsedTime = TASK3_PERIOD;
+    tasks[2].TickFct = &TickFct_drums;
 
-
+    tasks[3].period = TASK4_PERIOD;
+    tasks[3].state = button_init;
+    tasks[3].elapsedTime = TASK4_PERIOD;
+    tasks[3].TickFct = &TickFct_button;
 
     TimerSet(GCD_PERIOD);
     TimerOn();
-     serial_init(9600);
+    serial_init(9600);
      
     while (1) {
 
